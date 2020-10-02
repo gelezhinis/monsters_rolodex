6eb741544833d8c2cca9084f08d5cb1ab9fe2d59
gelezhinis <gelezhinis@gmail.com>
2020-10-02 15:36:53 +0300

    Updates

---
+++ precache-manifest.a5cfd62b0203588aa5aacf9c5c212efd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "167c279658fa9c9061c2438c0c6e37fe",
+    "url": "/monsters_rolodex/index.html"
+  },
+  {
+    "revision": "7773bff20699c3789183",
+    "url": "/monsters_rolodex/static/css/main.f4e5e5c5.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters_rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters_rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7773bff20699c3789183",
+    "url": "/monsters_rolodex/static/js/main.e74a7697.chunk.js"
+  },
+  {
+    "revision": "c4ea3b6a6d247bce6b14",
+    "url": "/monsters_rolodex/static/js/runtime-main.eefd6555.js"
+  }
+]);
